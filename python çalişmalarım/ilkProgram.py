b3c32ad4ceb19fba898a9d9bac5bf98b4c738d02
Safak993 <miracefendiyilmaz92371@gmail.com>
2024-10-01 20:43:19 +0300

    Çalısmalar

---
+++ python çalişmalarım/ilkProgram.py
@@ -0,0 +1,7 @@
+name = input("Senin adin ne?: ")
+print("Merhaba"+name)
+Yaş = int(input("Kaç Yaşindasin?: "))
+height = float(input("Boyunuz kaç? "))
+Yaş = Yaş + 1
+print("Sen "+str(Yaş)+"Yaşindayim")
+print("Sen "+str(height)+"cm boy")
