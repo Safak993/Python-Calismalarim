07b084728e95a34ae0286e318d6bdf5f2fef8a07
Safak993 <miracefendiyilmaz92371@gmail.com>
2025-12-20 00:41:36 +0300

    çalışma ezber kodlar

---
+++ python çalişmalarım/Fonksiyon calisma.py
@@ -0,0 +1,98 @@
+def harita_ciz(satir, sutun):
+    for i in range(satir):
+        for j in range(sutun):
+            if i == satir and j == sutun:
+                print("X", end=" ")
+            else:
+                print("*", end=" ")
+        print()
+
+
+# test
+print("döngü")
+d = 20
+a = 5
+
+
+def miracin_printi(text):
+    print(text)
+
+
+def sayi_listesi(turSayisi):
+    for i in range(turSayisi):
+        print(i * "*")
+
+
+# print("\nDöngü ve fonksiyon\n")
+# sayi_listesi(d)
+
+# print("\nelle\n")
+# print("*")
+# print("**")
+# print("***")
+# print("****")
+# print("*****")
+# print("******")
+# print("*******")
+# print("********")
+# print("*********")
+# print("**********")
+# print("***********")
+# print("************")
+# print("*************")
+# print("**************")
+
+# print("\nDöngü ile\n")
+# for i in range(d):
+#     print(i * "*")
+
+print(
+    "\n------------------------------------------------------------------------------\n"
+)
+# print("\nDöngü ile\n")
+# for i in range(d):
+#     print(i * "*")
+
+
+# print("\nelle\n")
+# print("*")
+# print("**")
+# print("***")
+# print("****")
+# print("*****")
+# print("******")
+# print("*******")
+# print("********")
+# print("*********")
+# print("**********")
+# print("***********")
+# print("************")
+# print("*************")
+# print("**************")
+
+
+# sayi_listesi(d)
+
+
+def aptal_print(text):
+    print("yazmicam 😡😡")
+
+
+def zeki_print(isim):
+    print(f"Merhaba {isim}, nasılsın?\n")
+    print("Ben senin kim olduğunu az önce öğrendim.\n")
+    print("Seninle tanıştığıma memnun oldum")
+
+
+print(
+    "\n------------------------------------------------------------------------------\n"
+)
+
+miracin_printi("mirac")
+
+print("mirac")
+
+aptal_print("mirac")
+
+zeki_print("mirac")
+print("mirac")
