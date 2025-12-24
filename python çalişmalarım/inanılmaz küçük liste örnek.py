137b384f0c2a2b6e75bc0ee9f5e0b5f6223e67c3
Safak993 <miracefendiyilmaz92371@gmail.com>
2025-12-24 20:32:38 +0300

    küçük konusma örneği

---
+++ python çalişmalarım/inanılmaz küçük liste örnek.py
@@ -0,0 +1,6 @@
+konusma = ["oyun oynuyom", "oyun oynuyorum", "oyundayım"]
+konus = input("\nkonuş\n(kullanıcı):")
+# print(konus)
+# print(konusma[0])
+if konus in (tuple(konusma)):
+    print("Ne güzel")
