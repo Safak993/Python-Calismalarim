f01713fc62c350733a040e5723425095e08e8226
Safak993 <miracefendiyilmaz92371@gmail.com>
2025-12-24 20:40:31 +0300

    Update inanılmaz küçük liste örnek.py

--- python çalişmalarım/inanılmaz küçük liste örnek.py
+++ python çalişmalarım/inanılmaz küçük liste örnek.py
@@ -1,6 +1,13 @@
-konusma = ["oyun oynuyom", "oyun oynuyorum", "oyundayım"]
-konus = input("\nkonuş\n(kullanıcı):")
-# print(konus)
-# print(konusma[0])
-if konus in (tuple(konusma)):
-    print("Ne güzel")
+#----Küçük liste örnekleri----#
+#----Listeler----#
+konusma = ["OYUN OYNUYOM", "OYUNDAYIM", "OYUN OYNUYORUM"] #listeleme
+konusma_2 = ["NASILSIN", "NABER", "İYİMİSİN"]
+#----Döngüleme----#
+while True:
+    konus = input("\nkonuş\n(kullanıcı):").upper() #büyük harfe çevirme
+    # print(konus)
+    # print(konusma[0])
+    if konus in (tuple(konusma)): # konusma 1 in cevapları
+        print("Güzel bende oyun severim")
+    if konus in (tuple(konusma_2)): #konusma 2 nin cevapları
+        print("İyi senden?")
